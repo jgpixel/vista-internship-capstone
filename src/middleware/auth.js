4908feb668ea7d58e926791b0782c5cacf60d1d3
Justin Gorelik <justin.gorelik@gmail.com>
2026-06-03 12:25:31 -0400

    Added stubbed User model

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,32 @@
-function auth(req, res, next) {
-  req.user = {
-    id: 'demo-user-id',
-    email: 'demo@example.com'
-  };
+import User from '../models/User.js'
 
-  next();
+async function auth(req, res, next) {
+  try {
+    const user = await User.findOneAndUpdate(
+      {
+        email: 'demo@example.com'
+      },
+      {
+        email: 'demo@example.com',
+        name: 'Demo User',
+      },
+      {
+        upsert: true,
+        returnDocument: 'after',
+        runValidators: true
+      }
+    );
+
+    req.user = {
+      id: user._id.toString(),
+      email: user.email,
+      name: user.name
+    };
+
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
 export default auth;
